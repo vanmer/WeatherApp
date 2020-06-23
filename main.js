58448acebc60f64d38ff78da7df977226658fa14
vanmer <60948275+vanmer@users.noreply.github.com>
2020-06-23 12:32:20 +0200

    cleaning code in app.js

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 // select the elements
 const dateElement = document.querySelector(".location .date");
 const searchBox = document.querySelector('.search-box');
+let city = document.querySelector('.location .city');
+let temp = document.querySelector('.current .temp');
+let weatherElement = document.querySelector('.current .weather');
+let hilow = document.querySelector('.hi-low');
 
+// api
 const api = {
   key: "46098c7949a688b482ae72bccedf24a3",
   base: "https://api.openweathermap.org/data/2.5/"
 }
-
 
 // display todays date
 const options = {
@@ -25,7 +29,6 @@ searchBox.addEventListener('keypress', setQuery);
 function setQuery(event) {
   if (event.keyCode == 13) {
     getResults(searchBox.value);
-    console.log(searchBox.value);
   }
 }
 
@@ -38,17 +41,8 @@ function getResults(query) {
 }
 
 function displayResults(weather) {
-  console.table(weather);
-  let city = document.querySelector('.location .city');
   city.innerText = `${weather.name}, ${weather.sys.country}`;
-
-  let temp = document.querySelector('.current .temp');
   temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
-
-  let weatherElement = document.querySelector('.current .weather');
   weatherElement.innerText = weather.weather[0].main;
-
-  let hilow = document.querySelector('.hi-low')
   hilow.innerText = `${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C`
-
 }
